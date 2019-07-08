badb3d8d147c2a07c8064357142cde41a7b32b31
saori.matsuura <sao.matsuura@gmail.com>
2019-07-08 13:10:43 +0000

    favorite3

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,19 @@
+class FavoritesController < ApplicationController
+  before_action :require_user_logged_in
+
+  def create
+    micropost=Micropost.find(params[:micropost_id])
+    current_user.favo(micropost)
+    flash[:success] = '投稿をお気に入り登録しました。'
+#    redirect_to users_path
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    micropost=Micropost.find(params[:micropost_id])
+    current_user.cancelfavorite(micropost)
+    flash[:success] = '投稿のお気に入り登録を解除しました。'
+#    redirect_to users_path
+    redirect_back(fallback_location: root_path)
+  end
+end
