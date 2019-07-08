6d8097a9d7ab201e8ca1a64a9e6aa64721b7ea8e
saori.matsuura <sao.matsuura@gmail.com>
2019-07-08 13:07:08 +0000

    favorite2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,10 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, presence: true, length: { maximum: 255 }
+
+#  has_many :reverses_of_favorite, class_name: 'Favarite', foreign_key: 'micropost_id'
+  has_many :favorites
+#  has_many :reverses_of_favorites, class_name: 'Favarite'
+  has_many :favoriteusers, through: :favorites, source: :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ApplicationRecord
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
 
+  has_many :favorites
+  has_many :favoriteposts, through: :favorites, source: :micropost 
+
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -28,5 +31,18 @@ class User < ApplicationRecord
 
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+
+  def favo(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def cancelfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favo?(micropost)
+    self.favoriteposts.include?(micropost)
   end
 end
